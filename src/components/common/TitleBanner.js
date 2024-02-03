a57fc938eebd706a090371052456a5250e675598
armandrismir <adrismir@outlook.com>
2024-02-03 15:18:20 -0800

    adjust banner img

--- src/components/common/TitleBanner.js
+++ src/components/common/TitleBanner.js
@@ -11,7 +11,7 @@ function TitleBanner(props)
     return(
         <div className="h-[90vh] w-screen flex justify-center items-center">
             <div style={bannerStyle} className="w-[90%] h-[60vh] p-20 flex items-center relative border-[10px] border-[#88BE22]">
-                <img src={imageSrc} alt="handshake" width="446px" className="absolute right-20 top-[-150px]"/>
+                <img src={imageSrc} alt="handshake" width="446px" className="absolute right-20"/>
                 <h1 className="text-white text-[80px] font-bold uppercase z-50">{children}</h1>
             </div>
         </div>
