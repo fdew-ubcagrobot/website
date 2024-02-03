02b87d19aa06a55a19aaedd174d13ced8c575ec9
armandrismir <adrismir@outlook.com>
2024-02-03 11:32:14 -0800

    Sponsorship page TitleBanner arguments

--- src/components/common/TitleBanner.js
+++ src/components/common/TitleBanner.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-function TitleBanner(props) {
+function TitleBanner(props) 
+{
+    const { fromColor, toColor, imageSrc, children} = props;
+
+    const bannerStyle = {
+        background: `linear-gradient(to top, ${fromColor}, ${toColor})`
+    };
+
     return(
         <div className="h-[90vh] w-screen flex justify-center items-center">
-            <div className="w-[90%] h-[60vh] p-20 flex items-center relative border-[10px] border-[#88BE22] bg-gradient-to-t from-[#4c59348a] to-[#345950]">
-                <img src={props.imageSrc} alt="handshake" width="446px" className="absolute right-20 top-[-150px]"/>
-                <h1 className="text-white text-[80px] font-bold uppercase z-50">{props.children}</h1>
+            <div style={bannerStyle} className="w-[90%] h-[60vh] p-20 flex items-center relative border-[10px] border-[#88BE22]">
+                <img src={imageSrc} alt="handshake" width="446px" className="absolute right-20 top-[-150px]"/>
+                <h1 className="text-white text-[80px] font-bold uppercase z-50">{children}</h1>
             </div>
         </div>
     );
--- src/components/sponsorship/sponsorship-s1.js
+++ src/components/sponsorship/sponsorship-s1.js
@@ -3,7 +3,7 @@ import StudentsWorking from "./studentsWorking.jpg";
 
 function SponsorshipS1(props) {
     return (
-        <div style={{ backgroundImage: `url(${StudentsWorking})` }} className="h-screen w-screen bg-cover bg-center">
+        <div style={{ backgroundImage: `url(${StudentsWorking})` }} className="min-h-screen w-screen bg-cover bg-center">
             <div className="h-screen w-screen bg-[#00000080] flex justify-center items-center p-40">
                 <p className="text-white text-[40px] text-center font-RobotoMono">Support the AgroBot team’s vision of efficient and environmental farming!</p>
             </div>
