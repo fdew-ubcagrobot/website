a71b4c4f88eeb5408619f8d4a356c29fa1cd1959
ericl574 <bea2792935061@gmail.com>
2024-02-02 18:32:58 -0800

    constant footer/sponsor update

---
+++ src/constant/footer.js
@@ -0,0 +1,24 @@
+export const teamLogo="./images/UBC_Agrobot.png";
+
+export const footerElement = [
+    'Home',
+    'Portfolio',
+    'Projects',
+    'Sponsorship',
+    'Recruitment',
+    'Contact Us',
+    'About Us',
+    'Shop'
+  ];
+
+  export const portfolioElement = [
+    'Achievements',
+    'Publication',
+    'Gallery'
+  ]
+  export const projectsElement = [
+    'Agrobot',
+    'AgroPonic'
+  ]
+
+ 
