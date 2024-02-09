8336ff7725d6b4d4dcd5ece34fea85881b080b88
kavyansh bansal <kavyansheduv@gmail.com>
2024-02-09 15:39:59 -0800

    added list inside footer

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -1,37 +1,38 @@
 import React from 'react';
-import {LOGO} from "../../assets";
+import { LOGO } from "../../assets";
 
 function Footer() {
-  return (
-    <footer className="footer bg-[#f8f7f1] text-[#8cbc24] p-25 ml-5 mb-20 mt-20">
-      <img src={LOGO} alt="UBC AgroBot Logo" className="float-left float-bottom mr-50"/>
-      <nav>
-        <ul className="list-none p-0">
-          <li className='inline mr-[30px] ml-10'><a href="/"><b>Home</b></a></li>
-          <li className='inline mr-[30px]'>
-            <a href="/portfolio"><b>Portfolio</b></a>
-            <ul className='hidden'>
-              <li className='sub'><a href="/achievements">Achievements</a></li>
-              <li className='sub'><a href="/publications">Publications</a></li>
-              <li className='sub'><a href="/gallery">Gallery</a></li>
-            </ul>
-          </li>
-          <li className='inline mr-[30px]'>
-            <a href="/projects"><b>Projects</b></a>
-            <ul className='hidden'>
-              <li className='sub'><a href="/agrobot">AgroBot</a></li>
-              <li className='sub'><a href="/agroponics">AgroPonics</a></li>
-            </ul>
-          </li>
-          <li className='inline mr-[30px]'><a href="/sponsorship"><b>Sponsorship</b></a></li>
-          <li className='inline mr-[30px]'><a href="/recruitment"><b>Recruitment</b></a></li>
-          <li className='inline mr-[30px]'><a href="/contactus"><b>Contact Us</b></a></li>
-          <li className='inline mr-[30px]'><a href="/aboutus"><b>About Us</b></a></li>
-          <li className='inline mr-[30px]'><a href="/shop"><b>Shop</b></a></li>
-        </ul>
-      </nav>
-    </footer>
-  );
+    return (
+        <footer className="footer bg-[#f8f7f1] text-[#8cbc24] p-25 ml-5 mb-20 mt-20">
+            <img src={LOGO} alt="UBC AgroBot Logo" className="float-left float-bottom mr-50" />
+            <nav>
+                <ul className="list-none p-0">
+                    <li className='inline mr-[30px] ml-10'><a href="/"><b>Home</b></a></li>
+                    <li className='inline mr-[30px]'>
+                        <a href="/portfolio"><b>Portfolio</b></a>
+                        <ul className='text-[#21130d] absolute mt-0 ml-[360px] bg-[#f8f7f1]'>
+                            <li className='sub'><a href="/achievements">Achievements</a></li>
+                            <li className='sub'><a href="/publications">Publications</a></li>
+                            <li className='sub'><a href="/gallery">Gallery</a></li>
+                        </ul>
+
+                    </li>
+                    <li className='inline mr-[30px]'>
+                        <a href="/projects"><b>Projects</b></a>
+                        <ul className='hidden'>
+                            <li className='sub'><a href="/agrobot">AgroBot</a></li>
+                            <li className='sub'><a href="/agroponics">AgroPonics</a></li>
+                        </ul>
+                    </li>
+                    <li className='inline mr-[30px]'><a href="/sponsorship"><b>Sponsorship</b></a></li>
+                    <li className='inline mr-[30px]'><a href="/recruitment"><b>Recruitment</b></a></li>
+                    <li className='inline mr-[30px]'><a href="/contactus"><b>Contact Us</b></a></li>
+                    <li className='inline mr-[30px]'><a href="/aboutus"><b>About Us</b></a></li>
+                    <li className='inline mr-[30px]'><a href="/shop"><b>Shop</b></a></li>
+                </ul>
+            </nav>
+        </footer>
+    );
 }
 
 export default Footer;
--- src/components/common/footerElements.js
+++ src/components/common/footerElements.js
@@ -1,76 +1,81 @@
-import styled from "styled-components";
-
-export const Home = styled(Link)`
-    color: ##00FF00;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height: 100%;
-    cursor: pointer;
-`;
-export const PORTFOLIO = styled(Link)`
-    color: ##00FF00;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height: 100%;
-    cursor: pointer;
-`;
-export const PROJECTS = styled(Link)`
-    color: ##00FF00;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height: 100%;
-    cursor: pointer;
-`;
-export const SPONSORSHIP = styled(Link)`
-    color: ##00FF00;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height: 100%;
-    cursor: pointer;
-`;
-export const RECRUITMENT = styled(Link)`
-    color: ##00FF00;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height: 100%;
-    cursor: pointer;
-`;
-export const CONTACT_US = styled(Link)`
-    color: ##00FF00;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height: 100%;
-    cursor: pointer;
-`;
-export const ABOUT_US = styled(Link)`
-    color: ##00FF00;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height: 100%;
-    cursor: pointer;
-`;
+/*
+This is a rough file using css which is not currently in use
+ */
 
 
-export const SHOP = styled(Link)`
-    color: ##00FF00;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height: 100%;
-    cursor: pointer;
-`;
+// import styled from "styled-components";
+
+// export const Home = styled(Link)`
+//     color: ##00FF00;
+//     display: flex;
+//     align-items: center;
+//     text-decoration: none;
+//     padding: 0 1rem;
+//     height: 100%;
+//     cursor: pointer;
+// `;
+// export const PORTFOLIO = styled(Link)`
+//     color: ##00FF00;
+//     display: flex;
+//     align-items: center;
+//     text-decoration: none;
+//     padding: 0 1rem;
+//     height: 100%;
+//     cursor: pointer;
+// `;
+// export const PROJECTS = styled(Link)`
+//     color: ##00FF00;
+//     display: flex;
+//     align-items: center;
+//     text-decoration: none;
+//     padding: 0 1rem;
+//     height: 100%;
+//     cursor: pointer;
+// `;
+// export const SPONSORSHIP = styled(Link)`
+//     color: ##00FF00;
+//     display: flex;
+//     align-items: center;
+//     text-decoration: none;
+//     padding: 0 1rem;
+//     height: 100%;
+//     cursor: pointer;
+// `;
+// export const RECRUITMENT = styled(Link)`
+//     color: ##00FF00;
+//     display: flex;
+//     align-items: center;
+//     text-decoration: none;
+//     padding: 0 1rem;
+//     height: 100%;
+//     cursor: pointer;
+// `;
+// export const CONTACT_US = styled(Link)`
+//     color: ##00FF00;
+//     display: flex;
+//     align-items: center;
+//     text-decoration: none;
+//     padding: 0 1rem;
+//     height: 100%;
+//     cursor: pointer;
+// `;
+// export const ABOUT_US = styled(Link)`
+//     color: ##00FF00;
+//     display: flex;
+//     align-items: center;
+//     text-decoration: none;
+//     padding: 0 1rem;
+//     height: 100%;
+//     cursor: pointer;
+// `;
+
+
+// export const SHOP = styled(Link)`
+//     color: ##00FF00;
+//     display: flex;
+//     align-items: center;
+//     text-decoration: none;
+//     padding: 0 1rem;
+//     height: 100%;
+//     cursor: pointer;
+// `;
