9f14c1642b770bc08ad83e36caa986ae48f70398
ericl574 <bea2792935061@gmail.com>
2024-02-09 14:05:32 -0800

    footer update

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -1,34 +1,33 @@
 import React from 'react';
-import './Footer.css'; 
 import {LOGO} from "../../assets";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <img src={LOGO} alt="UBC AgroBot Logo" className="logo"/>
+    <footer className="footer bg-[#f8f7f1] text-[#8cbc24] p-25 ml-5 mb-20 mt-10">
+      <img src={LOGO} alt="UBC AgroBot Logo" className="float-left float-bottom mr-50"/>
       <nav>
-        <ul>
-          <li><a href="/"><b>Home</b></a></li>
-          <li>
+        <ul className="list-none p-0">
+          <li className='inline mr-[30px] ml-10'><a href="/"><b>Home</b></a></li>
+          <li className='inline mr-[30px]'>
             <a href="/portfolio"><b>Portfolio</b></a>
-            <ul>
+            <ul className='hidden'>
               <li className='sub'><a href="/achievements">Achievements</a></li>
               <li className='sub'><a href="/publications">Publications</a></li>
               <li className='sub'><a href="/gallery">Gallery</a></li>
             </ul>
           </li>
-          <li>
+          <li className='inline mr-[30px]'>
             <a href="/projects"><b>Projects</b></a>
-            <ul>
+            <ul className='hidden'>
               <li className='sub'><a href="/agrobot">AgroBot</a></li>
               <li className='sub'><a href="/agroponics">AgroPonics</a></li>
             </ul>
           </li>
-          <li><a href="/sponsorship"><b>Sponsorship</b></a></li>
-          <li><a href="/recruitment"><b>Recruitment</b></a></li>
-          <li><a href="/contactus"><b>Contact Us</b></a></li>
-          <li><a href="/aboutus"><b>About Us</b></a></li>
-          <li><a href="/shop"><b>Shop</b></a></li>
+          <li className='inline mr-[30px]'><a href="/sponsorship"><b>Sponsorship</b></a></li>
+          <li className='inline mr-[30px]'><a href="/recruitment"><b>Recruitment</b></a></li>
+          <li className='inline mr-[30px]'><a href="/contactus"><b>Contact Us</b></a></li>
+          <li className='inline mr-[30px]'><a href="/aboutus"><b>About Us</b></a></li>
+          <li className='inline mr-[30px]'><a href="/shop"><b>Shop</b></a></li>
         </ul>
       </nav>
     </footer>
