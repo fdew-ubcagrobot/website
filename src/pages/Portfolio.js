9eb8258f0a90cb5e4465741bb4baba4cfdb85002
Apps247 <67941617+Apps247@users.noreply.github.com>
2024-02-10 00:17:33 +0000

    Portfolio pages

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -5,15 +5,15 @@ import Shake from "../components/sponsorship/shake.png";
 const Portfolio = () => {
     return (
         <div className="bg-[#F8F7F1] w-screen">
-            <TitleBanner imageSrc={Shake}>Portfolio</TitleBanner>
-            <SponsorshipS1></SponsorshipS1>
+            <TitleBanner fromColor="#919331" toColor="#9da38a" imageSrc={Shake}>Portfolio</TitleBanner>
+            {/* <SponsorshipS1></SponsorshipS1>
             <SponsorshipS2></SponsorshipS2>
             <SponsorshipS3>
-                Hello
+                AgroBots
             </SponsorshipS3>
             <SponsorshipS4>
-                Hello
-            </SponsorshipS4>
+                Key Features
+            </SponsorshipS4> */}
         </div>
     );
 };
