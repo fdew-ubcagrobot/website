2abdf3f8b4c3bc726fcb0ce7f10445d552b3e10e
Apps247 <67941617+Apps247@users.noreply.github.com>
2024-02-10 00:17:23 +0000

    Added imageAltText argument

--- src/components/common/TitleBanner.js
+++ src/components/common/TitleBanner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function TitleBanner(props) 
 {
-    const { fromColor, toColor, imageSrc, children} = props;
+    const { fromColor, toColor, imageSrc, imageAltText, children} = props;
 
     const bannerStyle = {
         background: `linear-gradient(to top, ${fromColor}, ${toColor})`
@@ -11,7 +11,7 @@ function TitleBanner(props)
     return(
         <div className="h-[90vh] w-screen flex justify-center items-center">
             <div style={bannerStyle} className="w-[90%] h-[60vh] p-20 flex items-center relative border-[10px] border-[#88BE22]">
-                <img src={imageSrc} alt="handshake" width="446px" className="absolute right-20 top-[-150px]"/>
+                <img src={imageSrc} alt={imageAltText || "Title Image"} width="446px" className="absolute right-20 top-[-20px]"/>
                 <h1 className="text-white text-[80px] font-bold uppercase z-50">{children}</h1>
             </div>
         </div>
