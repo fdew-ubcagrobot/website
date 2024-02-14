f7d40941308e3f5284ac9ee9be8fc357fec19c99
safzzz21 <zilansafaq@gmail.com>
2024-02-13 19:03:07 -0800

    changes in about us page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,15 @@
 import React from "react";
- 
-const AboutUs = () => {
+import { TitleBanner, PortfolioS1, StatsBar} from "../components";
+import { RecruitmentImage } from "../assets"; 
+import NotificationBanner from "../components/common/NotificationBanner";
+
+function AboutUs() {
     return (
-        <div>
-            <h1>Welcome to aboutus</h1>
+        <div className="bg-[#F8F7F1] w-screen">
+            <TitleBanner toColor="#2e533e" fromColor="#3b4e3a" imageSrc={RecruitmentImage} imageClassName="absolute right-0 bottom-0 w-4/12">About US</TitleBanner>
+           <NotificationBanner></NotificationBanner>
         </div>
     );
 };
- 
+
 export default AboutUs;
