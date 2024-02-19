fa079e0d349fbaff8cc3e8e8d3276df26d6feb02
ArmanDris <adrismir@gmail.com>
2024-02-19 01:25:40 -0800

    color tweak

--- src/components/home/HomeProjects.js
+++ src/components/home/HomeProjects.js
@@ -21,7 +21,7 @@ function HomeProjects() {
             <div className='w-[80vw] mx-auto text-[#3A3A3A]'>
                 <h1 className='font-Inter text-[48px] py-8'>Projects</h1>
                 <div className="flex w-full">
-                    <div className='w-[49%] bg-[#F8F7F1] rounded-[16px] p-8 py-12 mr-[2%]'>
+                    <div className='w-[49%] bg-[#CFEF94] rounded-[16px] p-8 py-12 mr-[2%]'>
                         <div className='flex w-full'>
                             <h2 className='mx-[5%] text-[32px] font-bold'>AgroBot</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
@@ -33,7 +33,7 @@ function HomeProjects() {
                             <div className='w-[45%] mx-[2.5%] my-[2.5%] rounded-[14px]'><Img imgSrc={HomeAutomation}>Automation</Img></div>
                         </div>
                     </div>
-                    <div className='w-[49%] bg-[#F8F7F1] rounded-[16px] p-8 py-12'>
+                    <div className='w-[49%] bg-[#CFEF94] rounded-[16px] p-8 py-12'>
                         <div className='flex w-full'>
                             <h2 className='mx-[5%] text-[32px] font-bold'>AgroPonics</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
