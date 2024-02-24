e9cbdd19877d12a74ed5acb12e72705becf2fc5d
Apps247 <67941617+Apps247@users.noreply.github.com>
2024-02-24 09:57:20 +0000

    Changed dropdown menu to toggle on hover

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -4,86 +4,100 @@ import { useState } from "react";
 // import icons
 import { HiOutlineMenu } from "react-icons/hi";
 
-
 import { navLinks } from "../../constant";
 import { agrobot } from "../../assets";
 
 const Navbar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [issubMenuOpen, setIssubMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [issubMenuOpen, setIssubMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-         // Close the mobile menu when toggling the portfolio dropdown
-        setIssubMenuOpen(false);
-};
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    // Close the mobile menu when toggling the portfolio dropdown
+    setIssubMenuOpen(false);
+  };
+
+  const togglesubMenu = () => {
     
+    setIssubMenuOpen(!issubMenuOpen);
+    // Close the portfolio dropdown menu when toggling the dropdown
+    setIsMenuOpen(false);
+  };
 
-    const togglesubMenu = () => {
-        setIssubMenuOpen(!issubMenuOpen);
-         // Close the portfolio dropdown menu when toggling the dropdown
-         setIsMenuOpen(false);
-};
-        
-    
-    return (
-        <header className="h-[10vh] bg-[#F8F7F1] w-full sticky top-0 left-0 right-0 border-b-2 z-[100]">
-            <nav className="h-full py-4 md:px-8 px-4">
-                <div className="h-full flex items-center justify-between">
-                    <div className="h-full font-bold cursor-pointer text-black ">
-                        <img className="h-full" src={agrobot} alt="UBC Agrobot" />
-                    </div>
+  return (
+    <header className="h-[10vh] bg-[#F8F7F1] w-full sticky top-0 left-0 right-0 border-b-2 z-[100]">
+      <nav className="h-full py-4 md:px-8 px-4">
+        <div className="h-full flex items-center justify-between">
+          <div className="h-full font-bold cursor-pointer text-black ">
+            <img className="h-full" src={agrobot} alt="UBC Agrobot" />
+          </div>
 
-                    <ul className="list-none lg:flex items-center gap-3 hidden">
-                        {navLinks.map((nav) => (
-                            <li key={nav.id} className="block text-[#8cbc24] py-2 px-4">
-                                {nav.id === "Portfolio" && (
-                                    <div className="dropdown">
-                                        <button className="dropbtn" onClick={togglesubMenu}>{nav.id} ▼</button>
-                                        {issubMenuOpen && (
-                                            <div className="dropdown-content">
-                                                <a href="/agrobot">Agrobot</a>
-                                                <a href="/agroponics" style={{ display: "block" }}>Agroponics</a> 
-                                            </div>
-                                        )}
-                                    </div>
-                                )}
-                                {nav.id !== "Portfolio" && (
-                                    <a href={`${nav.link}`}>{nav.id}</a>
-                                )}
-                            </li>
-                        ))}
-                    </ul>
-
-                {/* contact me button */}
-        
-            
-    
-
-                {/* menu button for small devices */}
-                    <button onClick={toggleMenu} className="lg:hidden text-[#8cbc24] text-3xl ">
-                        <HiOutlineMenu />
+          <ul className="list-none lg:flex items-center gap-3 hidden">
+            {navLinks.map((nav) => (
+              <li key={nav.id} className="block text-[#8cbc24] py-2 px-4">
+                {nav.dropdown && (
+                  <div
+                    className="dropdown"
+                    onMouseEnter={() => setTimeout(togglesubMenu, 500)}
+                    onMouseLeave={() => setTimeout(togglesubMenu, 500)}
+                  >
+                    <button
+                      className="dropbtn"
+                      onClick={() => {
+                        window.location.href = nav.link;
+                      }}
+                    >
+                      {nav.id} ▼
                     </button>
+                    {issubMenuOpen && (
+                      <div className="dropdown-content">
+                        <a href="/agrobot">Agrobot</a>
+                        <a href="/agroponics" style={{ display: "block" }}>
+                          Agroponics
+                        </a>
+                      </div>
+                    )}
+                  </div>
+                )}
+                {!nav.dropdown && <a href={`${nav.link}`}>{nav.id}</a>}
+              </li>
+            ))}
+          </ul>
 
-                </div>
+          {/* contact me button */}
 
-                {/* nav items for small devices */}
-                {
-                    isMenuOpen && (
-                        <div className="mt-4 bg-[#8cbc24] text-white rounded py-4">
-                            <a href="/" className="block hover:text-grey-400 py-2 px-4">Home</a>
-                            <a href="/" className="block hover:text-grey-400 py-2 px-4">Portfolio</a>
-                            <a href="/" className="block hover:text-grey-400 py-2 px-4">Sponsorship</a>
-                            <a href="/" className="block hover:text-grey-400 py-2 px-4">Recruitment</a>
-                            <a href="/" className="block hover:text-grey-400 py-2 px-4">About us</a>
-                        </div>
-                    )
-                }
+          {/* menu button for small devices */}
+          <button
+            onClick={toggleMenu}
+            className="lg:hidden text-[#8cbc24] text-3xl "
+          >
+            <HiOutlineMenu />
+          </button>
+        </div>
 
-            </nav>
-
-        </header>
-    );
-}
+        {/* nav items for small devices */}
+        {isMenuOpen && (
+          <div className="mt-4 bg-[#8cbc24] text-white rounded py-4">
+            <a href="/" className="block hover:text-grey-400 py-2 px-4">
+              Home
+            </a>
+            <a href="/" className="block hover:text-grey-400 py-2 px-4">
+              Portfolio
+            </a>
+            <a href="/" className="block hover:text-grey-400 py-2 px-4">
+              Sponsorship
+            </a>
+            <a href="/" className="block hover:text-grey-400 py-2 px-4">
+              Recruitment
+            </a>
+            <a href="/" className="block hover:text-grey-400 py-2 px-4">
+              About us
+            </a>
+          </div>
+        )}
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
