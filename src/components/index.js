6a007888fa074a3e2e5436444c7fc52bb8f3570c
Apps247 <67941617+Apps247@users.noreply.github.com>
2024-02-24 10:29:41 +0000

    Added publications section

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ import { TitleBanner } from "./common";
 import { SponsorshipS1, SponsorshipS2, SponsorshipS3 } from './sponsorship';
 import { HomeBanner, OurMission, HomeProjects } from "./home";
 import { Sponsors } from './common'
-import { InfoParagraph, StatsBar, PortfolioImageGallery, PortfolioProjects } from './portfolio';
+import { InfoParagraph, StatsBar, PortfolioImageGallery, PortfolioProjects, Publications } from './portfolio';
 import { mailForm, sideBar } from "./contactUs";
 import { AgroPonicsS1, AgroPonicsS2, AgroPonicsS3, AgroPonicsS4, AgroPonicsS5, Agroponicsnest, } from './agroponics';
 import { AgroBotIntro, AgroBotKeyFeatures, AgroBotSubteams, AgroBotWantToHelp, } from './agrobot';
@@ -21,6 +21,7 @@ export {
     StatsBar,
     PortfolioImageGallery,
     PortfolioProjects,
+    Publications,
     mailForm,
     sideBar,
     AgroPonicsS1,
--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -1,11 +1,13 @@
 import InfoParagraph from "./portfolio-s1.js";
 import StatsBar from "./statsBar.js";
 import PortfolioImageGallery from "./imageGallery.js";
+import Publications from "./publications.js";
 import PortfolioProjects from "./projects.js";
 
 export {
     InfoParagraph,
     StatsBar,
     PortfolioImageGallery,
+    Publications,
     PortfolioProjects,
 }
