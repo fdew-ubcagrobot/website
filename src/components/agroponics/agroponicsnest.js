a48e44b621a9ab4cadbb0eba63d8fb46d1b893fd
kavyansh bansal <kavyansheduv@gmail.com>
2024-02-28 20:19:28 -0800

    mobile view agroponics added

--- src/components/agroponics/agroponicsnest.js
+++ src/components/agroponics/agroponicsnest.js
@@ -11,17 +11,42 @@ function Agroponicsnest() {
             </div>
 
             {/* Middle section (text and image) */}
-            <div className='w-full h-[50vh] flex items-center' >
+            <div className='w-full h-[50vh] flex flex-col-reverse sm:flex-row items-center'>
                 {/* Left side (text) */}
-                <p className='flex-1 h-full text-[18px] font-Inter tracking-wide flex items-center justify-center' style={{ background: 'linear-gradient(114.8deg, #597A00 23.7%, #072600 76.36%)', borderRadius: '0px', color: 'white', padding: '50px' }}>
+                <p className='flex-1 h-full text-[18px] font-Inter tracking-wide flex items-center justify-center sm:justify-start mobile-text' style={{
+                    background: 'linear-gradient(114.8deg, #597A00 23.7%, #072600 76.36%)', borderRadius: '0px', color: 'white', padding: '50px'
+                }}>
                     We hope to develop efficient and self-sufficient systems that can be implemented in a variety of locations including the UBC campus. Our first prototype is being built on the roof of the nest.
                 </p>
 
                 {/* Right side (image) */}
                 <div className='flex-1 h-full' style={{ width: '90%' }}>
-                    <img src={AgroBotWihTeamOnGrass} className='w-full h-full' alt="AgroBot with Team on Grass" style={{ width: '100%' }} />
+                    <img src={AgroBotWihTeamOnGrass} className='w-full h-full check-img' alt="AgroBot with Team on Grass" style={{ width: '100%' }} />
                 </div>
             </div>
+
+            {/* Media query for mobile view */}
+            <style>
+                {`
+                    @media (max-width: 750px) {
+                        .flex-col-reverse {
+                            flex-direction: column;
+                           
+                        }
+                        .mobile-text {
+                            width: 90%;
+                        
+                        }
+                        .check-img {
+                            height : 79%;
+                        }
+                        
+                        
+                        
+                    }
+                   
+                `}
+            </style>
         </div>
     );
 };
