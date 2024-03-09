1b1b6a691da01434c639bee356758ecf6744fe22
Apps247 <67941617+Apps247@users.noreply.github.com>
2024-03-09 20:38:56 +0000

    Made Responsive

--- src/components/recruitment/recruitment-s4.js
+++ src/components/recruitment/recruitment-s4.js
@@ -4,21 +4,21 @@ import { FaLinkedin } from "react-icons/fa";
 
 
 const RecruitmentS4 = () => {
-  // TODO: Constantize text, add icons
+  // TODO: Constantize text
   return (
-    <div className="text-center mx-auto px-auto md:max-w-[60%] flex flex-wrap justify-around">
+    <div className="text-center mx-auto px-auto max-w-[90%]  md:max-w-[60%] flex flex-wrap justify-around">
       <h1 className="text-2xl my-auto py-auto text-[#3A3A3A] text-center">
         Follow us on social media for the latest updates:{" "}
       </h1>
       <a
-        className="text-lime-500 underline"
+        className="text-lime-500 underline pt:2 md:pt-0"
         href="https://www.instagram.com/ubcagrobots"
         target="_blank"
       >
         <FaInstagram size={40} />
       </a>{" "}
       <a
-        className="text-lime-500 underline"
+        className="text-lime-500 underline pt:2 md:pt-0"
         href="https://www.linkedin.com/company/ubc-agrobot"
         target="_blank"
       >
