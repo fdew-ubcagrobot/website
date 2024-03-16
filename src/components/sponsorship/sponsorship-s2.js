2aabcffba6129a81adf4b25d3cd4facb5b6f52e5
armandrismir <adrismir@outlook.com>
2024-03-15 18:39:26 -0700

    Made sponsorship section mobile friendly

--- src/components/sponsorship/sponsorship-s2.js
+++ src/components/sponsorship/sponsorship-s2.js
@@ -16,11 +16,11 @@ function SponsorshipS2() {
 
     return (
         <div id="sponsorshipDiv" className="min-h-[55vh] my-[7.5%] w-[80%] mx-auto">
-            <div className="flex justify-center">
-                <div className="w-[47.5%] relative inline-block mr-[5%] style={{ maxWidth: '47.5%' }}">
+            <div className="flex justify-center my-16 md:my-0">
+                <div className="w-0 md:w-[47.5%] relative inline-block mr-[5%] style={{ maxWidth: '47.5%' }}">
                     <img src={TeamPhoto} alt="Team" className="box-shadow absolute top-0 left-0 z-10 rounded-[14px]" style={{ width: '100%', height: 'auto' }} />
                 </div>
-                <div className="w-[47.5%] flex flex-col justify-center">
+                <div className="w-full md:w-[47.5%] flex flex-col justify-center">
                     <h2 className=" text-4xl pb-10">{processedS2Title}</h2>
                     <p className="pb-10">{S2BenefitsParagraph}</p>
                     <div className="flex justify-center">
--- src/components/sponsorship/sponsorship-s3.js
+++ src/components/sponsorship/sponsorship-s3.js
@@ -13,7 +13,7 @@ function SponsorshipS3() {
 
     return (
         <div style={{ backgroundImage: `url(${GreenHouse})` }} className="w-full bg-cover bg-center">
-            <div className="p-[7.5%] flex justify-between items-stretch bg-[#00000080]">
+            <div className="p-[7.5%] flex flex-col lg:flex-row justify-between items-stretch bg-[#00000080]">
                 <TierBanner tierElements={SponsorLevels.find(level => level.name === 'Bronze')} starImg={BronzeStar} tierColor="#CD7F32" style={{ height: tallestTierBannerHeight }} />
                 <TierBanner tierElements={SponsorLevels.find(level => level.name === 'Silver')} starImg={SilverStar} tierColor="#707070" style={{ height: tallestTierBannerHeight }} />
                 <TierBanner tierElements={SponsorLevels.find(level => level.name === 'Gold')} starImg={GoldStar} tierColor="#FFD700" style={{ height: tallestTierBannerHeight }} />
