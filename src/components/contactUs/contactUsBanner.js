bbb5f11da207aac5f73c1b217670be40c47ffb66
Skidlez <96402323+korihuen@users.noreply.github.com>
2024-03-15 23:38:51 -0700

    contact us design finished

--- src/components/contactUs/contactUsBanner.js
+++ src/components/contactUs/contactUsBanner.js
@@ -1,22 +1,29 @@
-import React, { Component } from 'react';
-import { mailPhoto } from '../../assets';
+// import React, { Component } from 'react';
+// import { mailPhoto } from '../../assets';
 
-class ContactUsBanner extends Component {
-  render() {
-    const { fromColor, toColor, imageClassName } = this.props;
+// class ContactUsBanner extends Component {
+//   render() {
+//     const {isMobile } = this.props;
 
-    // computer display
-    return (
-      <div className="h-auto w-full flex justify-center" style={{height: '200px'}}>
-          <div className="h-[70%] w-[80%] bg-[#88BE22] mt-8 flex flex-col items-center justify-center">
-            <div className="h-[90%] w-[98%] bg-[#006A60] flex items-center justify-center" style={{ background: 'linear-gradient(to bottom, #006A60, #00B894)' }}>
-              <h1 className="uppercase text-[48px] text-[#FFFFFF] font-bold mr-4">Contact Us </h1>
-              <img src={mailPhoto} alt="mailPhoto" className="h-[250px] w-[250px] mb-8 ml-4" />
-            </div>
-          </div>
-      </div>
-    );
-  }
-}
+//     if(!isMobile) {
+//       return (
+//         <div className="h-auto w-full flex justify-center" style={{height: '173px', width: '1013'}}>
+//           <div className="h-[90%] w-[80%] bg-[#88BE22] flex items-center justify-center rounded-md mb-10" style={{ background: 'linear-gradient(to bottom, #007165, #00b491)' }}>
+//               <h1 className="uppercase text-[48px] text-[#FFFFFF] font-bold mr-4">Contact Us </h1>
+//               <img src={mailPhoto} alt="mailPhoto" className="h-[250px] w-[250px] mb-8 ml-4" />
+//           </div>
+//         </div>
+//       );
+//     }
+//       return (
+//         <div className="w-full flex flex-col justify-center items-center mt-[4vh] md:mt-[10vh] mb-[4vh] md:mb-[10vh]" style={{height: '180px'}}>
+//           <div className="w-[90%] md:w-[80%] h-[20vh] flex items-center justify-center rounded-[14px]" style={{ background: 'linear-gradient(to bottom, #007165, #00b491)' }}>
+//               <h1 className="text-white text-2xl md:text-5xl font-bold uppercase z-50">Contact Us </h1>
+//               <img src={mailPhoto} alt="mailPhoto" className="h-[250px] w-[250px] mb-10 ml-4" />
+//           </div>
+//         </div>
+//       );
+//   }
+// }
 
-export default ContactUsBanner;
+// export default ContactUsBanner;
--- src/components/contactUs/mailForm.js
+++ src/components/contactUs/mailForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { EmailSideIcon, SocialSideIcon, LocationSideIcon } from "../../assets";
+import { AgrobotEmailIcon, AgrobotFacebookIcon, AgrobotInstagramIcon,AgrobotLinkedInIcon, AgrobotLocationIcon } from "../../assets";
 import { APPLICATION_LINK } from "../../constant/recruitment";
 import { MdChevronRight } from "react-icons/md";
 
@@ -10,14 +10,16 @@ function MailForm(props) {
   const [message, setMessage] = useState("");
   const { isMobile } = props;
 
+  
+
   if (!isMobile) {
     return (
       <div
-        className="mx-auto bg-[#F8F7F1]  border border-gray-300 rounded-md max-w-screen-md "
-        style={{ display: "flex" }}
+        className="mx-auto bg-[#F8F7F1] border border-gray-300 rounded-md p-4 w-4/5 flex items-center justify-center"
+        style={{ display: "flex", height: "55vh", width: "75%"}}
       >
-        <div style={{ width: "85%" }}>
-          <div className="w-4/5 h-full p-4">
+        <div style={{ width: "120%" }}>
+          <div className="w-4/5 max-h-full p-4">
             <div className="flex flex-col items-left justify-left w-full h-full">
               <h1 className="text-3xl font-bold text-left text-[#4C5934] ml-4">
                 Message Us
@@ -63,26 +65,58 @@ function MailForm(props) {
         </div>
         <div style={{ width: "40%" }}>
           <div className="flex flex-wrap justified-center items-left flex-col w-full">
-            <div className="w-[140px] h-[120px] bg-[#F8F7F1] border border-gray-300 rounded-md mt-6">
-              <img
-                src={EmailSideIcon}
-                alt="EmailSideIcon"
-                className="h-fit w-fit"
-              />
+            <div className="w-[180px] h-[140px] bg-[#F8F7F1] border border-gray-300 rounded-md">
+            <a target="_blank" href="mailto:ubcagrobot@gmail.com">
+                <img
+                  src={AgrobotEmailIcon}
+                  alt="EmailSideIcon"
+                  className="w-[50px] h-[40px] mt-3 mx-auto "
+                />
+              </a>
+              <h1 className="text-[18px] text-center font-bold mt-2">Email</h1>
+              <p className="text-[12px] text-[#3A3A3A] font-bold roboto-custom text-center mt-1">ubcagrobot@gmail.com</p>
+            
             </div>
-            <div className="w-[140px] h-[120px] bg-[#F8F7F1] border border-gray-300 rounded-md mt-6">
-              <img
-                src={SocialSideIcon}
-                alt="EmailSideIcon"
-                className="h-fit w-fit"
-              />
+            <div className="w-[180px] h-[140px] bg-[#F8F7F1] border border-gray-300 rounded-md mt-6" style={{position: "relative"}}>
+              <h1 className="text-[18px] text-center font-bold mt-3">Social Media</h1>
+              <p className="text-[12px] text-[#3A3A3A] font-bold roboto-custom text-center mt-2">Feel free to connect with us on Social Media! </p>
+              <div className="flex" style={{position: "absolute", bottom: 7, right: 14}}>
+              <a target="_blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                  <img
+                    src={AgrobotFacebookIcon}
+                    alt="FacebookIcon"
+                    className="w-[40px] h-[40px] mr-3 mr-1 mx-auto"
+                  />
+                  </a>
+                <a target="_blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                  <img
+                    src={AgrobotInstagramIcon}
+                    alt="InstagramIcon"
+                    className="w-[40px] h-[40px] mr-1 mx-auto"
+                  />
+                </a>
+                <a target="_blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                  <img
+                    src={AgrobotLinkedInIcon}
+                    alt="LinkedInIcon"
+                    className="w-[40px] h-[40px] ml-3 mx-auto"
+                  />
+                </a>
+              </div>
             </div>
-            <div className="w-[140px] h-[120px] bg-[#F8F7F1] border border-gray-300 rounded-md mt-6">
-              <img
-                src={LocationSideIcon}
-                alt="EmailSideIcon"
-                className="h-fit w-fit"
-              />
+              <div className="w-[180px] h-[140px] bg-[#F8F7F1] border border-gray-300 rounded-md mt-6">
+                <a target="_blank" href="https://maps.app.goo.gl/Wi2BrKJZQ9gQ2Bdx5">
+                  <img
+                    src={AgrobotLocationIcon}
+                    alt="LocationIcon"
+                    className="w-[45px] h-[38px] mt-1 mx-auto"
+                  />
+                </a>
+                <h1 className="text-[18px] text-center font-bold">Location</h1>
+                <p className="text-[11px] text-[#3A3A3A] font-bold roboto-custom text-center" > University of British Columbia </p>
+                <p className="text-[11px] text-[#3A3A3A] font-bold roboto-custom text-center"> 2370 E Mall </p>
+                <p className="text-[11px] text-[#3A3A3A] font-bold roboto-custom text-center"> Vancouver, BC </p>
+                <p className="text-[11px] text-[#3A3A3A] font-bold roboto-custom text-center"> V6T 1Z3 </p>
             </div>
           </div>
         </div>
@@ -90,36 +124,36 @@ function MailForm(props) {
     );
   } else {
     return (
-      <div className="w-4/5 h-full bg-[#4C5934] mx-auto rounded-md p-4 lg:p-8">
+      <div className="w-4/5 h-full bg-[#F8F7F1] mx-auto rounded-md lg:p-8">
         <div className="flex flex-col items-center justify-center w-full h-full">
-          <h1 className="text-3xl font-bold text-center text-white mb-4">
+          <h1 className="text-3xl font-bold text-center text-[#4C5934] mb-2 mt-2">
             Message Us!
           </h1>
           <form className="flex flex-col items-center justify-center w-full">
             <input
               type="text"
               placeholder="Name"
-              className="w-full lg:w-4/5 h-12 px-4 my-2 border-2 border-gray-300 rounded-md"
+              className="w-[80%] lg:w-4/5 h-12 px-4 my-2 border-2 border-gray-300 rounded-md"
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
             <input
               type="email"
               placeholder="Email"
-              className="w-full lg:w-4/5 h-12 px-4 my-2 border-2 border-gray-300 rounded-md"
+              className="w-[80%] lg:w-4/5 h-12 px-4 my-2 border-2 border-gray-300 rounded-md"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
             <input
               type="subject"
               placeholder="Subject"
-              className="w-full lg:w-4/5 h-12 px-4 my-2 border-2 border-gray-300 rounded-md"
+              className="w-[80%] lg:w-4/5 h-12 px-4 my-2 border-2 border-gray-300 rounded-md"
               value={subject}
               onChange={(e) => setSubject(e.target.value)}
             />
             <textarea
               placeholder="Message"
-              className="w-full lg:w-4/5 h-48 px-4 py-2 my-2 border-2 border-gray-300 rounded-md"
+              className="w-[80%] lg:w-4/5 h-48 px-4 py-2 my-2 border-2 border-gray-300 rounded-md"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
             />
