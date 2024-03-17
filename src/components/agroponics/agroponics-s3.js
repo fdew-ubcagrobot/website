75878526c108602b84a92601fe5c3fb278eff560
ericl574 <bea2792935061@gmail.com>
2024-03-17 14:39:08 -0700

    Update agroponics-s3.js

--- src/components/agroponics/agroponics-s3.js
+++ src/components/agroponics/agroponics-s3.js
@@ -7,7 +7,7 @@ function AgroPonicsS3() {
         <div className='ml-[10%] mb-[10%] w-full h-full'>
             <SectionHeader>Chemical Balancing System</SectionHeader>
             <div className="mt-[3%] relative w-[40%] h-[65vh] justify-center items-center shadow-2xl flex text-white rounded-[30px] bg-gradient-to-br from-[#90bc24] from-23% to-[#072600] to-76.36%">
-                <div className="absolute items-center justify-center h-[700px] w-auto max-w-[80%] max-h-[85vh]">
+                <div className="absolute flex items-center justify-center h-[700px] w-auto max-w-[80%] max-h-[85vh]">
                     <img src={HomeChemicalBalancing} alt="HomeChemicalBalancing" className="inset-0 rounded-[30px] bg-[#d8d4dc]" />
                 </div>
             </div>
