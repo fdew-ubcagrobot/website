34b9da7ffa43394169348a6830bfe7b51928a751
joshua <joshuac2@students.cs.ubc.ca>
2024-04-03 15:05:35 -0700

    made specification section in Agrobots page more mobile friendly by changing flex direction to column

--- src/components/agrobot/AgroBotSpecifications.js
+++ src/components/agrobot/AgroBotSpecifications.js
@@ -30,28 +30,29 @@ function AgroBotSpecifications() {
     return (
         <div className="w-[80%] mx-auto my-[20vh]">
             <div className="w-full rounded-[14px] p-10 text-white my-[5vh] bg-slate-800">
-                <h1 className="text-white text-[32px] sm:text-[48px] capitalize">Specifications</h1>
-                <div className="flex my-4 py-5 justify-evenly">
-                    <img className="mx-4 w-1/3 h-auto object-cover rounded-xl" src={temp_robot_blueprint} alt="blueprint of robot showing dimensions"></img>
-                    <div className="mx-4 w-1/2">
+                <h1 className="pb-5 mb-1 text-white text-[32px] sm:text-[48px] capitalize md:pb-0">Specifications</h1>
+                
+                <div className="flex flex-col md:flex-row md:my-4 md:py-5 md:justify-evenly">
+                    <img className="mx-4 md:w-1/3 h-auto object-cover rounded-xl" src={temp_robot_blueprint} alt="blueprint of robot showing dimensions"></img>
+                    <div className="mx-4 py-5 md:w-1/2">
                         <SpecificationText category="Dimensions" descriptionPoints={dimensions}></SpecificationText>
                         <div className="py-10"><hr></hr></div>
                         <SpecificationText category="Speeds" descriptionPoints={speeds}></SpecificationText>
                     </div>
                 </div>
 
-                <div className="flex my-4 py-10 justify-evenly">
-                    <div className="mx-4 py-5 w-1/2">
+                <div className="flex flex-col-reverse md:flex-row md:my-4 md:py-5 md:justify-evenly">
+                    <div className="mx-4 py-5 md:w-1/2">
                         <SpecificationText category="Power Consumption" descriptionPoints={powerConsumption}></SpecificationText>
                         <div className="py-10"><hr></hr></div>
                         <SpecificationText category="Operational Range" descriptionPoints={operationalRange}></SpecificationText>
                     </div>
-                    <img className="mx-4 w-1/3 h-auto object-cover rounded-xl" src={temp_circuitry} alt="circuitboard"></img>
+                    <img className="mx-4 md:w-1/3 h-auto object-cover rounded-xl" src={temp_circuitry} alt="circuitboard"></img>
                 </div>
 
-                <div className="flex my-4 py-5 justify-evenly">
-                    <img className="mx-4 w-1/3 h-auto object-cover rounded-xl" src={temp_maintenance} alt="woman doing maintenance"></img>
-                    <div className="mx-4 py-5 w-1/2">
+                <div className="flex flex-col md:flex-row md:my-4 md:py-5 md:justify-evenly">
+                    <img className="mx-4 md:w-1/3 h-auto object-cover rounded-xl" src={temp_maintenance} alt="woman doing maintenance"></img>
+                    <div className="mx-4 py-5 md:w-1/2">
                         <SpecificationText category="Weather Resistance" descriptionPoints={weatherResistance}></SpecificationText>
                         <div className="py-10"><hr></hr></div>
                         <SpecificationText category="Maintenance" descriptionPoints={maintenance}></SpecificationText>
