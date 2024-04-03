0c1c305901741254190e375bd47e771e9dea606c
armandrismir <adrismir@outlook.com>
2024-04-03 16:07:47 -0700

    Mobile support for WantToHelp

--- src/components/common/WantToHelp.js
+++ src/components/common/WantToHelp.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { TeamDiscussion } from "../../assets";
 import { MdChevronRight } from 'react-icons/md';
+import { SectionHeader } from "../../components";
 
 
 function WantToHelp() {
+    
+    const gradient = { background: `linear-gradient(35deg, rgba(207,239,148,1) 0%, rgba(207,239,148,0.75) 35%, rgba(207,239,148,0.75) 65%, rgba(207,239,148,1) 100%)` }
+
     return (
         <div className='w-[80%] mx-auto my-[20vh]'>
-            <h2 className='text-4xl'>Interested in joining the team?</h2>
-            <div className='w-full flex my-[5vh]'>
-                <div className='w-[49%] mr-[2%] flex flex-col justify-center items-center bg-[#d5eea7c2] rounded-[14px] py-6'>
-                    <p className='w-[60%] text-[24px]'>Members will work towards solving a real-world problem, go through the engineering design process, and develop technical skills in mechanical, electrical and software design. </p>
-                    <button className='bg-[#88BE22] text-white px-4 py-2 rounded-[14px] flex items-center justify-center font-bold my-4' onClick={() => window.location.href = '/recruitment'}>Apply Here <MdChevronRight/></button>
+            <SectionHeader>Want to help?</SectionHeader>
+            <div className='w-full flex justify-between my-4'>
+                <div style={gradient} className='w-full lg:w-[49%] rounded-[14px] flex flex-col justify-center items-center p-8'>
+                    <p className='w-[60%] text-md lg:text-lg text-justify mb-4'>Members will work towards solving a real-world problem, go through the engineering design process, and develop technical skills in mechanical, electrical and software design. </p>
+                    <button className='bg-[#202020] text-white px-4 py-2 rounded-[14px] flex items-center justify-center font-bold' onClick={() => window.location.href = '/recruitment'}>Apply Here <MdChevronRight/></button>
                 </div>
-                <div className='w-[49%] flex justify-center items-center'>
+                <div className='hidden lg:flex w-[49%]'>
                     <img src={TeamDiscussion} alt='modular-chassis' className='rounded-[14px] w-full'></img>
                 </div>
             </div>
