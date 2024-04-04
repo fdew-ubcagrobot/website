49b1fa9e7e9c8af55b393ba523c6478719135dfd
joshua <joshuac2@students.cs.ubc.ca>
2024-04-03 18:12:36 -0700

    fixed mobile formatting of agrobot page intro text

--- src/components/common/ProjectInstance.js
+++ src/components/common/ProjectInstance.js
@@ -5,7 +5,7 @@ function ProjectInstance(props) {
 
 	return (
 		<div className='w-[80%] m-auto my-[10%]'>
-			<section className="flex">
+			<section className="flex flex-col md:flex-row">
 				<div className='text-[22px] text-justify w-full md:w-[45%]'>
 					<div className="">
 						<h1 className="text-[24px] font-bold text-[#88BE22]">PROJECT_{projectNum}</h1>
@@ -15,7 +15,7 @@ function ProjectInstance(props) {
                         {text}
 					</p>
 				</div>
-				<div className="hidden md:flex justify-end items-center w-[50%] ml-[5%] p-10">
+				<div className="mt-5 p-5 md:flex md:justify-end md:items-center md:w-[50%] md:ml-[5%] md:p-10">
 					<img src={img} alt="Hydroponics System" className="shadow-2xl rounded-[20px] bg-[#d8d4dc] w-full" />
 				</div>
 			</section>
