107ae7b1877bad0f6903fafee0b632f985dff57b
armandrismir <adrismir@outlook.com>
2024-06-04 16:13:46 -0700

    New ourmission section!

--- src/components/home/OurMission.js
+++ src/components/home/OurMission.js
@@ -1,82 +1,32 @@
-import React, { useState } from "react";
-import { HomeOurMissionRecycle, HomeOurMissionStrawberry, HomeOurMissionStocks, StudentsWorking, AgroBotWihTeamOnGrass, TeamPhoto } from '../../assets';
-import { SectionHeader, } from '../../components';
-import { MdChevronRight, MdChevronLeft } from 'react-icons/md';
-import * as t from "../../constant/home";
+import LeafIcon from "../../assets/image/brown-leaf-icon.png"
 
-function MissionBox(props) {
-    const text = props.text;
-
-    const angle = useState(() => Math.random() * 360 - 180)[0];
-
-    const gradient = {
-        background: `linear-gradient(${angle}deg, rgba(207,239,148,0.4) 0%, rgba(207,239,148,0.7) 100%)`
-    }
-
-    return (
-        <div style={gradient} className='bg-[#cfef94] rounded-[14px] my-8'>
-            <div className='flex justify-center items-center mt-[-1rem]'>
-                <h4 className="text-black font-bold p-2 px-4 bg-[#F8F7F1] rounded-[8px] text-center inline-block]">{props.children}</h4>
-            </div>
-            <div className="flex w-full p-8 pt-4">
-                <div className=" min-w-[60px] max-w-[30%] mr-[5%] flex justify-center items-center">
-                    <img src={props.imgSrc} alt='mission' className='w-full'></img>
-                </div>
-                <p className="flex-grow">{text}</p>
-            </div>
-        </div>
-    );
+function KeyCards() {
+	return (
+		<div className="text-[#873807] w-full h-full flex justify-center items-center flex-col">
+			<img src={LeafIcon} className="w-[160px]" />
+			<p className="text-[36px] text-center">agrobot helps reduce pesticide waste by 60%</p>
+			<div className="flex w-[70%] mt-6">
+				<span className="w-full h-[20px] mx-2 bg-[#873807] rounded-full" />
+				<span className="w-full h-[20px] mx-2 bg-[#8738072a] rounded-full" />
+				<span className="w-full h-[20px] mx-2 bg-[#8738072a] rounded-full" />
+			</div>
+		</div>
+	);
 }
 
 function OurMission() {
 
-    const images = [AgroBotWihTeamOnGrass, TeamPhoto, StudentsWorking];
-
-    const [currentImgIndex, setCurrentImgIndex] = useState(0);
-
-    function prevImg() {
-        setCurrentImgIndex(currentImgIndex => {
-            if (currentImgIndex === 0)
-                return images.length - 1;
-            return currentImgIndex - 1;
-        });
-    }
-
-    const nextImg = () => {
-        setCurrentImgIndex(index => {
-            if (index === images.length - 1)
-                return 0;
-            return index + 1;
-        });
-    };
-
-    return (
-        <div className="w-full pb-[10vh] flex justify-center items-center">
-            <div className='w-[80vw] mx-auto mt-16'>
-                <SectionHeader className='mt-16'>{t.OM_title}</SectionHeader>
-                <p className='mt-2 mb-8'>{t.OM_subtitle}</p>
-                <div id="ourMission" className="flex flex-col md:flex-row w-full">
-                    <div className="w-full md:w-[40%] flex flex-col">
-                        <MissionBox imgSrc={HomeOurMissionRecycle}    text={t.OM_box1_text}>{t.OM_box1_title}</MissionBox>
-                        <MissionBox imgSrc={HomeOurMissionStrawberry} text={t.OM_box2_text}>{t.OM_box2_title}</MissionBox>
-                        <MissionBox imgSrc={HomeOurMissionStocks}     text={t.OM_box3_text}>{t.OM_box3_title}</MissionBox>
-                    </div>
-                    <div className="w-full md:w-[60%] flex flex-col justify-center items-center p-[2%]">
-                        <img alt='our mission' src={images[currentImgIndex]} className="object-cover h-full rounded-[14px]" />
-                        <div className='flex'>
-                            <MdChevronLeft onClick={prevImg} size={48} />
-                            <div className='flex justify-center items-center'>
-                                {images.map((img, index) => (
-                                    <div key={index} className={`${index === currentImgIndex ? 'bg-black' : 'bg-[#F8F7F1]'} w-6 h-2 m-1 rounded-sm`} />
-                                ))}
-                            </div>
-                            <MdChevronRight onClick={nextImg} size={48} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="w-full h-[100vh] bg-[#77be2099] px-[10%] flex justify-center items-center">
+			<div className="w-[50%]">
+				<h1 className="mb-4 font-bold text-[48px] text-[#2E1B0F]">We want to make farming  greener</h1>
+				<p className="text-[36px] text-[#2E1B0F]">Using our AgroBot technology dignissim leo ante, quis blandit orci laoreet ut. Suspendisse ornare id justo ac fringilla. Duis quis molestie risus, sed vulputate velit. Duis fringilla pharetra viverra. </p>
+			</div>
+			<div className="w-[40%] ml-[10%]">
+				<KeyCards />
+			</div>
+		</div>
+	);
 }
 
 export default OurMission;
