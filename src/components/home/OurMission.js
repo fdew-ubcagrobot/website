8acedd64f20bfcbea2c66cae64b46b8cd1e5b2c9
armandrismir <adrismir@outlook.com>
2024-06-11 12:51:03 -0700

    Working but sub par version of cards animation

--- src/components/home/OurMission.js
+++ src/components/home/OurMission.js
@@ -8,28 +8,77 @@ const RectangleAnimation = () => {
 	const cardWidth = 400
 	const cardHeight = 250
 
-	const centerX = cardWidth/2
-	const centerY = height-(cardHeight/2)
+	const centerX = cardWidth / 2
+	const centerY = height - (cardHeight / 2)
 	const offsetX = (width - cardWidth) / 2
 	const offsetY = (height - cardHeight) / 2
 
 	const homePos = { left: centerX, top: centerY }
-	const offsetOne = { left: centerX + offsetX, top: centerY-offsetY }
-	const offsetTwo = { left: centerX + 2*offsetX, top: centerY-2*offsetY }
-	  
+	const offsetOne = { left: centerX + offsetX, top: centerY - offsetY }
+	const offsetTwo = { left: centerX + 2 * offsetX, top: centerY - 2 * offsetY }
+
+	const bottomPos = { left: centerX, top: height + cardHeight / 2 }
+	const bottomLeftPos = { left: width + cardWidth / 2, top: height + cardHeight / 2 }
+	const topRightPos = { left: width + cardWidth / 2, top: 0 }
+
 	const brownCard = useSpring({
 		from: homePos,
-		to: homePos,
+		to: async next => {
+			while (true) {
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(bottomPos)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(bottomLeftPos)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(topRightPos)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(offsetTwo)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(offsetOne)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(homePos)
+			}
+		}
 	});
 
 	const greenCard = useSpring({
 		from: offsetOne,
-		to: offsetOne,
+		to: async next => {
+			while (true) {
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(homePos)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(bottomPos)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(bottomLeftPos)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(topRightPos)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(offsetTwo)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(offsetOne)
+			}
+		}
 	})
 
 	const redCard = useSpring({
 		from: offsetTwo,
-		to: offsetTwo,
+		to: async next => {
+			while (true) {
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(offsetOne)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(homePos)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(bottomPos)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(bottomLeftPos)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(topRightPos)
+				await new Promise(resolve => setTimeout(resolve, 2000))
+				await next(offsetTwo)
+			}
+		}
 	})
 
 	return (
@@ -48,18 +97,18 @@ const RectangleAnimation = () => {
 			</animated.div>
 
 			<animated.div className="absolute w-[400px] h-[250px] bg-[#1CF33E] rounded-md"
-				style={{ 
-					transform: 'translate(-50%, -50%)', 
-					...greenCard, 
+				style={{
+					transform: 'translate(-50%, -50%)',
+					...greenCard,
 				}}
 			>
 				<h2 className="font-bold p-10  text-white">AGROBOT</h2>
 			</animated.div>
 
 			<animated.div className="absolute w-[400px] h-[250px] bg-[#2E1B0F] rounded-md"
-				style={{ 
-					transform: 'translate(-50%, -50%)', 
-					...brownCard, 
+				style={{
+					transform: 'translate(-50%, -50%)',
+					...brownCard,
 				}}
 			>
 				<h2 className="font-bold p-10  text-white">AGROBOT</h2>
